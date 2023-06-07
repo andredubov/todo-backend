10dbf3f015022aa2f222896730dd790aaa3cc210
Andrei Dubov <av.dubov@yandex.ru>
2023-06-07 21:29:43 +0400

    fixed intial sql script

--- schema/init.sql
+++ schema/init.sql
@@ -18,14 +18,14 @@ CREATE TABLE todo_items
     id serial not null unique,
     title varchar(255) not null,
     description varchar(255),
-    done bolean not null default false
+    done boolean not null default false
 );
 
 CREATE TABLE lists_items
 (
     id serial not null unique,
-    list_id int references todo_lists(id) on delete cascade not null, 
-    item_id int references todo_items(id) on delete cascade not null,
+    list_id int references todo_lists(id) on delete cascade not null,
+    item_id int references todo_items(id) on delete cascade not null
 );
 
 CREATE TABLE users_lists
