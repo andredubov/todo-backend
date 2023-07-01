a01614af6f89606bcb216c19564dd0d271e3b4f9
Andrei Dubov <av.dubov@yandex.ru>
2023-07-01 16:46:20 +0400

    added todo list test for creation a new todo list

---
+++ internal/repository/list_test.go
@@ -0,0 +1,81 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/andredubov/todo-backend/internal/domain"
+	"github.com/dvln/testify/assert"
+	"github.com/jmoiron/sqlx"
+)
+
+func TestList_Create(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	dbx := sqlx.NewDb(db, "sqlmock")
+	todoListRepository := NewPostgresTodoListRepository(dbx)
+
+	type (
+		args struct {
+			userId   int
+			todoList domain.TodoList
+		}
+
+		mockBehavior func(args args, id int)
+
+		test struct {
+			name         string
+			input        args
+			mockBehavior mockBehavior
+			wantId       int
+			wantErr      bool
+		}
+	)
+
+	tests := []test{
+		{
+			name: "Ok",
+			input: args{
+				userId: 1,
+				todoList: domain.TodoList{
+					Id:          1,
+					Title:       "title",
+					Description: "description",
+				},
+			},
+			mockBehavior: func(args args, id int) {
+				mock.ExpectBegin()
+				rows := sqlmock.NewRows([]string{"id"}).AddRow(id)
+				itemsTableQuery, usersListsTableQuery := fmt.Sprintf("INSERT INTO %s", todoListTable), fmt.Sprintf("INSERT INTO %s", usersListsTable)
+				mock.ExpectQuery(itemsTableQuery).WithArgs(args.todoList.Title, args.todoList.Description).WillReturnRows(rows)
+				mock.ExpectExec(usersListsTableQuery).WithArgs(args.userId, args.todoList.Id).WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit()
+			},
+			wantId:  1,
+			wantErr: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			test.mockBehavior(test.input, test.wantId)
+
+			gotId, err := todoListRepository.Create(context.TODO(), test.input.todoList, test.input.userId)
+			if test.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, test.wantId, gotId)
+			}
+
+			assert.NoError(t, mock.ExpectationsWereMet())
+		})
+	}
+}
