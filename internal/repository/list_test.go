fba8be3a714e3a19aece462887188fa1b7057f04
Andrei Dubov <av.dubov@yandex.ru>
2023-07-01 17:25:40 +0400

    added todo list test to test getting todo lists by id

--- internal/repository/list_test.go
+++ internal/repository/list_test.go
@@ -160,5 +160,64 @@ func TestList_GetByUserId(t *testing.T) {
 			assert.NoError(t, mock.ExpectationsWereMet())
 		})
 	}
+}
 
+func TestList_GetById(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	dbx := sqlx.NewDb(db, "sqlmock")
+	todoListRepository := NewPostgresTodoListRepository(dbx)
+
+	type (
+		args struct {
+			userId     int
+			todoListId int
+		}
+
+		test struct {
+			name         string
+			mockBehavior func(args)
+			input        args
+			want         domain.TodoList
+			wantErr      bool
+		}
+	)
+
+	tests := []test{
+		{
+			name: "Ok",
+			mockBehavior: func(args args) {
+				rows := sqlmock.NewRows([]string{"id", "title", "description"}).AddRow(1, "title1", "description1")
+
+				query := fmt.Sprintf("SELECT (.+) FROM %s tl INNER JOIN %s ul on (.+) WHERE (.+)", todoListTable, usersListsTable)
+				mock.ExpectQuery(query).WithArgs(args.userId, args.todoListId).WillReturnRows(rows)
+			},
+			input: args{
+				userId:     1,
+				todoListId: 2,
+			},
+			want: domain.TodoList{Id: 1, Title: "title1", Description: "description1"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			test.mockBehavior(test.input)
+
+			got, err := todoListRepository.GetById(context.TODO(), test.input.userId, test.input.todoListId)
+			if test.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, test.want, got)
+			}
+
+			assert.NoError(t, mock.ExpectationsWereMet())
+		})
+	}
 }
