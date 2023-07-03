629cfbdacfdf9270330093cb01d364376a6a7468
Andrei Dubov <av.dubov@yandex.ru>
2023-07-03 16:19:52 +0400

    added test to test getListByID endpoint (Empty)

--- internal/transport/http/v1/list_test.go
+++ internal/transport/http/v1/list_test.go
@@ -508,6 +508,33 @@ func TestHandler_getListByID(t *testing.T) {
 			expectedStatusCode:   http.StatusOK,
 			expectedResponseBody: "{\"id\":1,\"title\":\"title1\",\"description\":\"description1\"}\n",
 		},
+		{
+			enviroment: enviroment{
+				appEnv:               "local",
+				httpHost:             "localhost",
+				httpPort:             "8080",
+				postgresHost:         "localhost",
+				postgresPort:         "5432",
+				postgresDatabaseName: "postgres",
+				postgresUsername:     "postgres",
+				postgresPassword:     "qwerty",
+				postgressSSLMode:     "disable",
+				passwordSalt:         "salt",
+				jwtSigningKey:        "key",
+			},
+			name:   "Empty",
+			jwtTTL: time.Duration(5 * time.Minute),
+			delay:  time.Duration(0 * time.Millisecond),
+			input: args{
+				userId:     1,
+				todoListId: 2,
+			},
+			mockBehavior: func(s *mock_service.MockTodoList, args args) {
+				s.EXPECT().GetById(gomock.Any(), args.userId, args.todoListId).Return(domain.TodoList{}, errors.New(""))
+			},
+			expectedStatusCode:   http.StatusInternalServerError,
+			expectedResponseBody: "{\"message\": \"unable to get a todolist by id: \"}",
+		},
 	}
 
 	for _, test := range tests {
