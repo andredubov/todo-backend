ec6cf08d7590862f6edac509317b5fe672e3a4ed
Andrei Dubov <av.dubov@yandex.ru>
2023-07-03 19:38:24 +0400

    added test case to test  deleteListByID endpoint (Token Expired)

--- internal/transport/http/v1/list_test.go
+++ internal/transport/http/v1/list_test.go
@@ -924,6 +924,33 @@ func TestHandler_deleteListByID(t *testing.T) {
 			expectedStatusCode:   http.StatusOK,
 			expectedResponseBody: "{\"status\":\"success\"}\n",
 		},
+		{
+			enviroment: enviroment{
+				appEnv:               "local",
+				httpHost:             "localhost",
+				httpPort:             "8080",
+				postgresHost:         "localhost",
+				postgresPort:         "5432",
+				postgresDatabaseName: "postgres",
+				postgresUsername:     "postgres",
+				postgresPassword:     "qwerty",
+				postgressSSLMode:     "disable",
+				passwordSalt:         "salt",
+				jwtSigningKey:        "key",
+			},
+			name:   "Token Expired",
+			jwtTTL: time.Duration(1 * time.Millisecond),
+			delay:  time.Duration(1 * time.Second),
+			input: args{
+				userId:     1,
+				todoListId: 2,
+			},
+			mockBehavior: func(s *mock_service.MockTodoList, args args) {
+
+			},
+			expectedStatusCode:   http.StatusUnauthorized,
+			expectedResponseBody: "{\"message\": \"Token is expired\"}",
+		},
 	}
 
 	for _, test := range tests {
